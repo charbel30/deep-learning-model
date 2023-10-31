d2402d1ec5328c39efdbcffd9259ad269c0c07b8
charbel30 <charbeltannous30@gmail.com>
2023-10-31 12:37:13 -0400

    removed average from pipe

--- models/FeedForwardNet/model_3.py
+++ models/FeedForwardNet/model_3.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self, input_size):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size, 512)  # Increase the number of neurons
+        self.fc1 = nn.Linear(input_size, 512)  
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 128)
         self.fc4 = nn.Linear(128, 64)
